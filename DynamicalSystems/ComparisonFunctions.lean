49e19315a7502a39dc8bf80b99c5f6dc19853e2e
Moritz Doll <moritz.doll@googlemail.com>
2026-04-21 11:52:53 +1000

    stuff

--- DynamicalSystems/ComparisonFunctions.lean
+++ DynamicalSystems/ComparisonFunctions.lean
@@ -38,3 +38,46 @@ theorem MemKL.memK (hf : MemKL f) (x : ℝ) : MemK (f x) := by
   refine ⟨?_, hf.strictMono x, hf.zero x⟩
   have : Continuous f.uncurry := hf.cont
   fun_prop
+
+namespace MemK
+
+variable {f g : ℝ≥0 → ℝ≥0}
+
+theorem injective (hf : MemK f) : Function.Injective f :=
+  hf.strictMono.injective
+
+@[fun_prop]
+theorem comp (hf : MemK f) (hg : MemK g) : MemK (f ∘ g) where
+  cont := hf.cont.comp hg.cont
+  strictMono := hf.strictMono.comp hg.strictMono
+  zero := by simp [hf.zero, hg.zero]
+
+@[fun_prop]
+theorem invFun (hf : MemK f) (hf' : f.Bijective) : MemK f.invFun where
+  cont := by
+    rw [Metric.continuous_iff]
+    intro y₀ ε hε
+    set x₀ := f.invFun y₀
+    --set y₁ := f (x₀ - ε)
+    --set y₂ := f (x₀ + ε)
+    -- take δ := min (y₀ - y₁) (y₂ - y₀)
+    --apply?
+    sorry
+  strictMono := by
+    rw [StrictMono]
+    by_contra!
+    obtain ⟨y₁, y₂, hy, h⟩ := this
+    set x₁ := f.invFun y₁
+    set x₂ := f.invFun y₂
+    have hx : x₂ ≤ x₁ := by
+      sorry
+    have := hf.strictMono.monotone hx
+    sorry
+  zero := by
+    convert Function.leftInverse_invFun hf.injective 0
+    simp [hf.zero]
+
+end MemK
+
+
+#check f⁻¹
