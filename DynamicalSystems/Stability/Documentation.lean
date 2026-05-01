d7252e63d49fd9fc0ac8a26368ef497797da1ecd
Moritz Doll <moritz.doll@googlemail.com>
2026-05-02 00:01:47 +1000

    feat: write a bit about Lyapunov stability (#18)

--- DynamicalSystems/Stability/Documentation.lean
+++ DynamicalSystems/Stability/Documentation.lean
@@ -14,30 +14,59 @@ set_option linter.hashCommand false
 
 #doc (Manual) "Stability" =>
 
-We introduce several notions of stability.
+# Basic stability
+
+We start by introducing the most basic notions of stability and how to use them in Lean.
+This will include Lyapunov stability and asymptotic stability. We will illustrate how to
+employ LaSalle's invariance principle to prove asymptotic stability even if the Lyapunov
+function is not strictly decreasing everywhere.
 
 ```lean -show
-variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
+variable {α E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
 
 open scoped Topology
-open Filter Set
+open Filter Set Metric
 
-variable {Φ : ℝ → E → E}
+variable {Φ : ℝ → E → E} {x₀ : E} {s : Set E}
 ```
-Throught this document, we will assume that `E` is a normed space over `ℝ`.
+Throughout this document, we will assume that `E` is a normed space over `ℝ`.
 
-- Lyapunov stability
-- input to state stability
 
-Let $`l` be a {lean}`Filter` and $`Φ` be a map from $`ℝ × E` to $`E`.
-Then $`Φ` is {lean}`Filter.IsStableOn` the filter $`l` if for every `s ∈ l` there exists `s' ∈ l`,
-such that for all `x ∈ s'` the flow of `x` is contained in `s`.
+## Lyapunov stability
+Consider a dynamical system $`\dot{x} = f(x)` a subset $`s` is called stable if all trajectories
+that are close to $`s` stay close to $`s`. This condition can be naturally phrased in terms of
+{lean}`Filter`:
 
-The typical case is that `l` is given by the neighbourhood filter of `x₀ : E`. Then we recover the
-usual definition:
+{docstring Filter.IsStableOn}
+
+The typical case is that `l` is given by the neighbourhood filter {lean}`nhds x₀`. Then we recover
+the usual definition:
 ```lean
 example (x₀ : E) : (𝓝 x₀).IsStableOn Φ (Ici 0) ↔
     ∀ ε > 0, ∀ t₀ ≥ 0, ∃ δ > 0, ∀ x,
     ‖x - x₀‖ < δ → ∀ t ≥ t₀, dist ‖Φ t x - x₀‖ < ε := by
   sorry
+```
+
+More generally, we can take the set neighbourhood filter {lean}`nhdsSet s`, then we have
+```lean
+example (s : Set E) : (𝓝ˢ s).IsStableOn Φ (Ici 0) ↔
+    ∀ ε > 0, ∀ t₀ ≥ 0, ∃ δ > 0, ∀ x,
+    infDist x s < δ → ∀ t ≥ t₀, infDist (Φ t x) s < ε := by
+  sorry
+```
+
+## Asymptotic stability
+Asymptotic stability requires that an equilibrium point is Lyapunov stable and all trajectories
+converge to the equilibrium point. In Lean, we are not defining a predicate for this, so to state
+that a point is asymptotic stable, one simply writes that it is stable and all trajectories
+converge. We note that in the case of the filter {lean}`nhdsSet s`, we can check convergence
+similarly to the convergence of points
+```lean
+variable {f : α → E} {l : Filter α}
+
+example (hs₁ : IsCompact s) (hs₂ : Set.Nonempty s) :
+    Tendsto f l (𝓝ˢ s) ↔
+    ∀ ε > 0, ∀ᶠ x in l, infDist (f x) s < ε := by
+  exact tendsto_nhdsSet hs₁ hs₂
 ```
