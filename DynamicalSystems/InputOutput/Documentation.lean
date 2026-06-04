8f7934bcbf8692efe1baf1c7c5d628adaac38ed5
Moritz Doll <moritz.doll@googlemail.com>
2026-06-04 18:02:06 +1000

    more documentation

--- DynamicalSystems/InputOutput/Documentation.lean
+++ DynamicalSystems/InputOutput/Documentation.lean
@@ -25,13 +25,23 @@ der Schaft, in particular Chapter 1 and Chapter 2.
 
 {ref "lploc"}[Previously] we recalled the fundamentals of `Lp` spaces.
 
-## Technical preliminaries
+## Relations
 
-### Relations
+The analysis of input-output systems usually involves chaining multiple systems together in a
+feedback loop. While all practical application the feedback connection is again a well-defined
+input-output map, this is not necessarily the case in general. It would be possible to always
+assume that the feedback connection is well-defined, but this means that artificial looking
+assumptions have to be carried around for proofs that do not require them.
+To avoid talking about well-definedness, instead of using input-output maps, we use relations.
+
+A relation is simply a subset of the cartesian product of two sets.
+
+{docstring SetRel}
+
+Every function defines a relation via its graph {name}`Function.graph`.
 
 
-
-### Unbundled functions
+## Unbundled functions
 
 ## Causal maps
 
@@ -45,3 +55,7 @@ and if a relation is induced by a function, then these definitions agree:
 {docstring Function.graph_isCausal_iff_isCausal}
 
 ## Finite gain stability
+
+## Dissipation inequalities
+
+{docstring IsDissipativeWith}
