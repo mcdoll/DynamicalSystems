5f400c09f25ad9a76143341239a2782a31f653af
Moritz Doll <moritz.doll@googlemail.com>
2026-06-17 16:49:02 +1000

    delete files

--- DynamicalSystems/Mathlib/Analysis/ODE/Documentation.lean
+++ DynamicalSystems/Mathlib/Analysis/ODE/Documentation.lean
@@ -3,8 +3,6 @@ import VersoManual
 import DynamicalSystems.Mathlib.Analysis.ODE.Caratheodory
 import DynamicalSystems.Mathlib.Analysis.ODE.ExistUnique
 import DynamicalSystems.Mathlib.Analysis.ODE.GlobalExistence
-import DynamicalSystems.Mathlib.Analysis.ODE.Gronwall
-import DynamicalSystems.Mathlib.Analysis.ODE.PicardLindelof
 import DynamicalSystems.Mathlib.Analysis.ODE.Transform
 import DynamicalSystems.Mathlib.Analysis.ODE.UniformlyLocallyLipschitz
 
--- DynamicalSystems/Mathlib/Analysis/ODE/PicardLindelof.lean
+++
@@ -1,8 +0,0 @@
-/-
-Copyright (c) 2021 Yury Kudryashov. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yury Kudryashov, Winston Yin
--/
-module
-
-public import Mathlib.Analysis.ODE.PicardLindelof
